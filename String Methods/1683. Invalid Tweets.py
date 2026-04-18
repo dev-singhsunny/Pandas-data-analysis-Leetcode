1fcc110179e54c8da30c8e44580f3fff7d41ff91
Sunny Singh <so39sunny@gmail.com>
2026-04-18 23:18:01 +0530

    Added solution for Leetcode 1683

---
+++ String Methods/1683. Invalid Tweets.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    tweets = tweets[tweets['content'].apply(lambda x: True if len(x)> 15 else False)]
+    return tweets[['tweet_id']]
