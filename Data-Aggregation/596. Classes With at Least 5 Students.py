598adad3c33f391e81e77ee31bec5bc4e54a4a58
Sunny Singh <so39sunny@gmail.com>
2026-05-07 11:16:38 +0530

    Added solution for Leetcode 596

---
+++ Data-Aggregation/596. Classes With at Least 5 Students.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    courses = courses['class'].value_counts().reset_index()
+    return courses[courses['count'] >= 5]['class'].to_frame()
