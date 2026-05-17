e63033d7d78112e5544ea0fbdcfb9edd4118f726
Sunny Singh <so39sunny@gmail.com>
2026-05-17 23:42:42 +0530

    Added solution for Leetcode 607

---
+++ Data Integration/607. Sales Person.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    red_com_id = company[company['name'] == "RED"]['com_id']
+    orders = orders[orders['com_id'].isin(red_com_id)]['sales_id']
+    return sales_person[~(sales_person['sales_id'].isin(orders))]['name'].to_frame()    
